ebfac001f116f8cc8b64994df1209ed955458f05
Nick <nboober@terpmail.umd.edu>
2019-10-28 17:46:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(driver => 
+        driver.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
