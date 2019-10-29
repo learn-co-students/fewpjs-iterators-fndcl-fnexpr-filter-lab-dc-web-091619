c7cea7c53730e83be4322ad2262a73bfbfcd5152
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-29 18:28:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+function findMatching(drivers, name){
+  let matches = drivers.filter(driver => {return driver.toLowerCase() === name.toLowerCase()});
+  return matches;
+}
+
+function fuzzyMatch(drivers, name){
+  let nameLength = name.length;
+  let matches = drivers.filter(driver => {return driver.slice(0,nameLength) === name});
+  debugger
+  return matches;
+}
+
+function matchName(drivers, name){
+  let matches = drivers.filter(function(driver){
+    return driver.name === name
+  });
+  
+  return matches;
+}
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
